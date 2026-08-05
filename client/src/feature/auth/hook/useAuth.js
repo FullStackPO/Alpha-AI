370d645ac56a2bad64440a2ff141ed05f3cd7c18
Harsh Arya <harsharya8946@gmail.com>
2026-08-05 23:52:15 +0530

    api layer & Redux implementation

---
+++ client/src/feature/auth/hook/useAuth.js
@@ -0,0 +1,53 @@
+import { useDisptach } from 'react-redux'
+import { register, login, getMe } from '../services/auth.api'
+import { setError, setLoading, setUser } from '../auth.slice'
+
+
+export async function useAuth(){
+
+    const dispatch = useDisptach()
+
+    async function registerHandler({ username, email, password }){
+        try{
+            dispatch(setLoading(true))
+            const data = await register({ username, email, password })
+        }
+        catch(err){
+            dispatch(setError(err.response?.data?.message || "Registration Failed"))
+        }
+        finally{
+            dispatch(setLoading(false))
+        }
+    }
+
+    async function loginHandler({ email, password }){
+        try{
+            dispatch(setLoading(true))
+            const data = await login({ email, password })
+            dispatch(setUser(data))
+        }
+        catch(err){
+            dispatch(setError(err.response?.data?.message || " Login Failed"))
+        }
+        finally{
+            dispatch(setLoading(false))
+        }
+    }
+
+    async function getMeHandler(){
+        try{
+            dispatch(setLoading(true))
+            const data = await getMe()
+            dispatch(setUser(data))
+        }
+        catch(err){
+            dispatch(setError(err.response?.data?.message || "getMe failed"))
+        }
+        finally{
+            dispatch(setLoading(false))
+        }
+    }
+
+    return { registerHandler, loginHandler, getMeHandler }
+
+}
