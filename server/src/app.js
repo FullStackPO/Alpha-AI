8021dcc558ae97a1c5e81cb2d01ce36e9d824cde
Harsh Arya <harsharya8946@gmail.com>
2026-08-06 15:44:31 +0530

    client - server integration

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,18 @@ import express from 'express'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import handleError from './middleware/error.middleware.js'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}))
+app.use(morgan("dev"))
 
 app.use('/api/auth', authRouter)
 
