08489ed5d14921ce857b4f87803b26ca16557888
Harsh Arya <harsharya8946@gmail.com>
2026-08-18 00:18:58 +0530

    getChats, getMessage, deleteChat api's

--- server/src/controller/chat.controller.js
+++ server/src/controller/chat.controller.js
@@ -46,3 +46,87 @@ export async function sendMessage( req, res, next ){
   }
 
 }
+
+export async function getChats(req, res, next){
+  
+  try {
+
+    const user = req.user
+    
+    const chats = await chatModel.find({ user : user.id })
+    
+    res.status(200).json({
+      message : "Chat retrives successfully",
+      chats
+  })
+
+  } 
+  catch (err) {
+    next(err)  
+  }
+
+}
+
+export async function getMessage(req, res, next){
+
+  try {
+    
+    const { chatId } = req.params;
+
+    const chat = await chatModel.findOne({
+      _id : chatId,
+      user : req.user.id
+    })
+
+    if(!chat){
+      return res.status(404).json({
+        message : "chat not found."
+      })
+    }
+
+    const message = await messageModel.find({ chat : chatId })
+
+    res.status(200).json({
+      message : "message retrieved successfully",
+      message
+    })
+
+  } 
+  
+  catch (error){
+    next(error)
+  }
+
+}
+
+export async function deleteChat(req, res, next){
+
+  try {
+    
+    const chatId = req.params;
+
+    const chat = await chatModel.findOneAndDelete({
+      _id : chatId,
+      user : req.user.id
+    })
+
+    await messageModel.deleteMany({
+      chat: chatId
+    })
+
+    if(!chat){
+      return res.status(404).json({
+        message : "Chat not found"
+      })
+    }
+
+    res.status(200).json({
+      message : "Chat deleted Successfully"
+    })
+
+  } 
+  catch (error) {
+      next(error)
+  }
+
+}
--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,9 +1,16 @@
 import { Router } from 'express'
 import identifyUser from '../middleware/identifyUser.middleware.js'
-import { sendMessage } from '../controller/chat.controller.js'
+import { getChats, getMessage, sendMessage, deleteChat } from '../controller/chat.controller.js'
 
 const chatRouter = Router()
 
 chatRouter.post('/message', identifyUser, sendMessage)
+
+chatRouter.get('/', identifyUser, getChats)
+
+chatRouter.get('/:chatId/messages', identifyUser, getMessage)
+
+chatRouter.delete("/delete/:chatId", identifyUser, deleteChat)
+
 
 export default chatRouter
