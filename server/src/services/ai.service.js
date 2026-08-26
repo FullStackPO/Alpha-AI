3bc73078b2650847677602c0604f03f47d5d374c
Harsh Arya <harsharya8946@gmail.com>
2026-08-27 00:42:24 +0530

    searchInternetTool (agent)

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,6 +1,8 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 import { ChatMistralAI } from '@langchain/mistralai'
-import { AIMessage, HumanMessage, SystemMessage } from '@langchain/core/messages'
+import { AIMessage, HumanMessage, SystemMessage, tool, createAgent } from 'langchain'
+import { searchInternet } from './internet.service.js'
+import * as z from "zod"
 import 'dotenv/config'
 
 const geminiModel = new ChatGoogleGenerativeAI({
@@ -13,16 +15,43 @@ const mistralModel = new ChatMistralAI({
     apiKey : process.env.MISTRAL_API_KEY
 })
 
+const searchInternetTool = tool(
+    searchInternet,
+    {
+        name : "searchInternet",
+        description : "Use this tool to get the latest information from the internet",
+        schema : z.object({
+            query : z.string().describe("The search query to look up on the internet.")
+        })
+    }
+)
+
+const agent = createAgent({ 
+    model : mistralModel,
+    tools : [ searchInternetTool ]
+})
+
+
+
 export const generateResponse = async( message ) => {
-    const response = await geminiModel.invoke(message.map(msg=>{
+    const response = await agent.invoke({
+        messages : [
+            new SystemMessage(`
+                    You are a helpful and precise assistant for answering questions.
+                    If you don't know the answer, say you don't know.
+                    If the questions requires up-to-date information, use the "searchInternet" tool to get the
+                    latest information from the internet and then answer based on the search results.
+                `), 
+            ...(message.map(msg=>{
         if(msg.role == "user"){
             return new HumanMessage(msg.content)
         }
         else if(msg.role == "ai"){
             return new AIMessage(msg.content)
         }
-    }))
-    return response.text
+    }))]
+})
+    return response.messages[ response.messages.length - 1 ].text;
 }
 
 export const generateTitle = async(message) => {
--- server/src/services/internet.service.js
+++ server/src/services/internet.service.js
@@ -3,3 +3,11 @@ import  { tavily as Tavily } from '@tavily/core'
 const tavily = Tavily({
     apiKey : process.env.TAVILY_KEY
 })
+
+export const searchInternet = async({ query }) =>{
+    const result =  await tavily.search(query, {
+        maxResults : 5,
+        searchDepth : "basic"
+    })
+    return JSON.stringify(result)
+}
